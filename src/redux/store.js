1151c187f79c581241861a77e0271516012e35d2
YanaHorbachova <maya69877@gmail.com>
2021-07-28 15:21:40 +0300

    start

--- src/redux/store.js
+++ src/redux/store.js
@@ -49,15 +49,3 @@ const storeFunc = { store, persistor };
 
 export default storeFunc;
 
-//// WITHOUT REDUX TOOLKIT
-// import { createStore, combineReducers } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import phonebookReducer from "./phonebook/phonebook-reducer";
-
-// const rootReducer = combineReducers({
-//   phonebook: phonebookReducer,
-// });
-
-// const store = createStore(rootReducer, composeWithDevTools());
-
-// export default store;
